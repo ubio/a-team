fa78b391b3de86f5d919f98d11668d981724f6f3
Boris Okunskiy <boris@okunskiy.name>
2018-07-24 18:09:04 +0300

    Amend phrasing in error codes inspection

--- inspections/no-default-error-codes.js
+++ inspections/no-default-error-codes.js
@@ -16,9 +16,13 @@ module.exports = {
 function* inspect(script) {
     for (const info of script.collectErrorCodesInfo()) {
         if (DEFAULT_ERROR_CODES.includes(info.errorCode)) {
+            const isExpect = info.action && info.action.type === 'expect';
+            const level = isExpect ? 'warning' : 'error';
+            const message = isExpect ? `Default error code "${info.errorCode}" is used` :
+                `Default error code "${info.errorCode}" is not allowed`;
             yield {
-                level: 'warning',
-                message: `Default error code "${info.errorCode}" is not allowed`,
+                level,
+                message,
                 context: info.context,
                 action: info.action
             }
