e0cb0e81585f3148a71726d726599f899072cd74
Boris Okunskiy <boris@okunskiy.name>
2018-07-24 18:36:09 +0300

    Revisit custom pipes inspection

---
+++ inspections/check-custom-pipes.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+    scope: 'action',
+    name: 'No Untracked Pipes',
+    domains: [],
+    inspect
+};
+
+function* inspect(action) {
+    for (const pipe of action.allPipes()) {
+        if (pipe.type !== 'other/custom') {
+            continue;
+        }
+        const { label } = pipe.configuration;
+        const status = window.domains.assets.getPipeStatus(pipe);
+        switch (status) {
+            case 'UNTRACKED':
+                yield {
+                    level: 'error',
+                    message: `Untracked pipe "${label}" is not allowed`
+                };
+                break;
+            case 'MODIFIED':
+                yield {
+                    level: 'error',
+                    message: `Local modifications to pipe "${label}" are not allowed`
+                };
+                break;
+            case 'OUTDATED':
+                yield {
+                    level: 'warning',
+                    message: `Newer version of "${label}" pipe is available`
+                };
+                break;
+        }
+    }
+}
+
